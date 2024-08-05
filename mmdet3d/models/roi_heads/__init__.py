64ae660b1cfffd365a20f93a1814ff2b00258c74
nibauman <nibauman@ethz.ch>
2024-08-05 10:08:59 +0000

    pruned repo eval on mini runs

--- mmdet3d/models/roi_heads/__init__.py
+++ mmdet3d/models/roi_heads/__init__.py
@@ -7,13 +7,10 @@ from .part_aggregation_roi_head import PartAggregationROIHead
 from .point_rcnn_roi_head import PointRCNNRoIHead
 from .roi_extractors import (Single3DRoIAwareExtractor,
                              Single3DRoIPointExtractor, SingleRoIExtractor)
-from .quasi_dense_roi_head import QuasiDenseRoIHead
-from .track_heads import QuasiDenseEmbedHead
 
 __all__ = [
     'Base3DRoIHead', 'PartAggregationROIHead', 'PointwiseSemanticHead',
     'Single3DRoIAwareExtractor', 'PartA2BboxHead', 'SingleRoIExtractor',
     'H3DRoIHead', 'PrimitiveHead', 'PointRCNNRoIHead', 'H3DBboxHead',
-    'PointRCNNBboxHead', 'Single3DRoIPointExtractor', 'QuasiDenseRoIHead',
-    'QuasiDenseEmbedHead'
+    'PointRCNNBboxHead', 'Single3DRoIPointExtractor'
 ]
--- mmdet3d/models/roi_heads/roi_extractors/__init__.py
+++ mmdet3d/models/roi_heads/roi_extractors/__init__.py
@@ -2,9 +2,8 @@
 from mmdet.models.roi_heads.roi_extractors import SingleRoIExtractor
 from .single_roiaware_extractor import Single3DRoIAwareExtractor
 from .single_roipoint_extractor import Single3DRoIPointExtractor
-from .rotated_3d_roi_extractor import Rotated3DRoIExtractor
 
 __all__ = [
     'SingleRoIExtractor', 'Single3DRoIAwareExtractor',
-    'Single3DRoIPointExtractor', 'Rotated3DRoIExtractor'
+    'Single3DRoIPointExtractor'
 ]
