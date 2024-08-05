bb272d5faf79bf8df4a98740aaa26eb52e15b570
nibauman <nibauman@ethz.ch>
2024-08-05 13:12:48 +0000

    centerpoint config delete todos

--- configs/centerpoint/centerpoint_01voxel_second_secfpn_4x8_cyclic_20e_nus.py
+++ configs/centerpoint/centerpoint_01voxel_second_secfpn_4x8_cyclic_20e_nus.py
@@ -35,7 +35,7 @@ data_config = {
     # Augmentation
     'resize': (-0.06, 0.11),
     'rot': (-5.4, 5.4),
-    'flip': True,  #TURNED OFF FOR NOW
+    'flip': True,  
     'crop_h': (0.0, 0.0),
     'resize_test': 0.00,
 }
@@ -51,7 +51,6 @@ bda_aug_conf = dict(
     flip_dx_ratio=0.5,
     flip_dy_ratio=0.5)
 
-# TODO: Extend it by the radar data? Unless the stuff in _base_ already includes the Radar pointcloud data
 train_pipeline = [
     dict(
         type='PrepareImageInputs',
@@ -120,7 +119,6 @@ test_pipeline = [
 ]
 
 # This is only for the submission I think and does not change the loading of the data
-# per se
 input_modality = dict(
     use_lidar=True,
     use_camera=True,
@@ -132,10 +130,9 @@ share_data_config = dict(
     type=dataset_type,
     classes=class_names,
     modality=input_modality,
-    img_info_prototype='bevdet', # TODO: Change?
+    img_info_prototype='bevdet',
 )
 
-# TODO: I guess the .pkl should still be good?
 test_data_config = dict(
     pipeline=test_pipeline,
     ann_file=data_root_pkl + 'bevdetv2-nuscenes_infos_val.pkl')
@@ -162,162 +159,3 @@ for key in ['train', 'val', 'test']:
 # Sets eval interval, THIS HAS TO BE THE SAME as in the Checkpoint interval!
 evaluation = dict(interval=5)
 checkpoint_config = dict(interval=5)
-# fp16 = dict(loss_scale='dynamic')
-
-
-# dataset_type = 'NuScenesDataset'
-# data_root = 'data/nuscenes/'
-# data_root_pkl = 'data/nuscenes_pkl/'
-# file_client_args = dict(backend='disk')
-
-# db_sampler = dict(
-#     data_root=data_root,
-#     info_path=data_root_pkl + 'bevdetv2-nuscenes_dbinfos_train.pkl',
-#     rate=1.0,
-#     prepare=dict(
-#         filter_by_difficulty=[-1],
-#         filter_by_min_points=dict(
-#             car=5,
-#             truck=5,
-#             bus=5,
-#             trailer=5,
-#             construction_vehicle=5,
-#             traffic_cone=5,
-#             barrier=5,
-#             motorcycle=5,
-#             bicycle=5,
-#             pedestrian=5)),
-#     classes=class_names,
-#     sample_groups=dict(
-#         car=2,
-#         truck=3,
-#         construction_vehicle=7,
-#         bus=4,
-#         trailer=6,
-#         barrier=2,
-#         motorcycle=6,
-#         bicycle=6,
-#         pedestrian=2,
-#         traffic_cone=2),
-#     points_loader=dict(
-#         type='LoadPointsFromFile',
-#         coord_type='LIDAR',
-#         load_dim=5,
-#         use_dim=[0, 1, 2, 3, 4],
-#         file_client_args=file_client_args))
-
-# train_pipeline = [
-#     dict(
-#         type='LoadPointsFromFile',
-#         coord_type='LIDAR',
-#         load_dim=5,
-#         use_dim=5,
-#         file_client_args=file_client_args),
-#     dict(
-#         type='LoadPointsFromMultiSweeps',
-#         sweeps_num=9,
-#         use_dim=[0, 1, 2, 3, 4],
-#         file_client_args=file_client_args,
-#         pad_empty_sweeps=True,
-#         remove_close=True),
-#     dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
-#     #dict(type='ObjectSample', db_sampler=db_sampler),
-#     dict(
-#         type='GlobalRotScaleTrans',
-#         rot_range=[-0.3925, 0.3925],
-#         scale_ratio_range=[0.95, 1.05],
-#         translation_std=[0, 0, 0]),
-#     dict(
-#         type='RandomFlip3D',
-#         sync_2d=False,
-#         flip_ratio_bev_horizontal=0.5,
-#         flip_ratio_bev_vertical=0.5),
-#     dict(type='PointsRangeFilter', point_cloud_range=point_cloud_range),
-#     dict(type='ObjectRangeFilter', point_cloud_range=point_cloud_range),
-#     dict(type='ObjectNameFilter', classes=class_names),
-#     dict(type='PointShuffle'),
-#     dict(type='DefaultFormatBundle3D', class_names=class_names),
-#     dict(type='Collect3D', keys=['points', 'gt_bboxes_3d', 'gt_labels_3d'])
-# ]
-# test_pipeline = [
-#     dict(
-#         type='LoadPointsFromFile',
-#         coord_type='LIDAR',
-#         load_dim=5,
-#         use_dim=5,
-#         file_client_args=file_client_args),
-#     dict(
-#         type='LoadPointsFromMultiSweeps',
-#         sweeps_num=9,
-#         use_dim=[0, 1, 2, 3, 4],
-#         file_client_args=file_client_args,
-#         pad_empty_sweeps=True,
-#         remove_close=True),
-#     dict(
-#         type='MultiScaleFlipAug3D',
-#         img_scale=(1333, 800),
-#         pts_scale_ratio=1,
-#         flip=False,
-#         transforms=[
-#             dict(
-#                 type='GlobalRotScaleTrans',
-#                 rot_range=[0, 0],
-#                 scale_ratio_range=[1., 1.],
-#                 translation_std=[0, 0, 0]),
-#             dict(type='RandomFlip3D'),
-#             dict(
-#                 type='PointsRangeFilter', point_cloud_range=point_cloud_range),
-#             dict(
-#                 type='DefaultFormatBundle3D',
-#                 class_names=class_names,
-#                 with_label=False),
-#             dict(type='Collect3D', keys=['points'])
-#         ])
-# ]
-# # construct a pipeline for data and gt loading in show function
-# # please keep its loading function consistent with test_pipeline (e.g. client)
-# eval_pipeline = [
-#     dict(
-#         type='LoadPointsFromFile',
-#         coord_type='LIDAR',
-#         load_dim=5,
-#         use_dim=5,
-#         file_client_args=file_client_args),
-#     dict(
-#         type='LoadPointsFromMultiSweeps',
-#         sweeps_num=9,
-#         use_dim=[0, 1, 2, 3, 4],
-#         file_client_args=file_client_args,
-#         pad_empty_sweeps=True,
-#         remove_close=True),
-#     dict(
-#         type='DefaultFormatBundle3D',
-#         class_names=class_names,
-#         with_label=False),
-#     dict(type='Collect3D', keys=['points'])
-# ]
-
-# test_data_config = dict(
-#     pipeline=test_pipeline,
-#     ann_file=data_root_pkl + 'bevdetv2-nuscenes_infos_val.pkl')
-
-# data = dict(
-#     train=dict(
-#         type='CBGSDataset',
-#         dataset=dict(
-#             type=dataset_type,
-#             data_root=data_root,
-#             ann_file=data_root_pkl + 'bevdetv2-nuscenes_infos_train.pkl',
-#             pipeline=train_pipeline,
-#             classes=class_names,
-#             test_mode=False,
-#             use_valid_flag=True,
-#             # we use box_type_3d='LiDAR' in kitti and nuscenes dataset
-#             # and box_type_3d='Depth' in sunrgbd and scannet dataset.
-#             box_type_3d='LiDAR')),
-#     val=test_data_config,
-#     test=dict(pipeline=test_pipeline, classes=class_names))
-
-# # Sets eval interval, THIS HAS TO BE THE SAME as in the Checkpoint interval!
-# evaluation = dict(interval=5)
-# checkpoint_config = dict(interval=5)
